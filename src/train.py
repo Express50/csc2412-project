052e2f7256fc6744a70cbffdab3741a848478a3a
David Landsman <david.landsman@mail.utoronto.ca>
2020-11-15 18:08:43 -0500

    Fix training code to work with sentiment data

--- src/train.py
+++ src/train.py
@@ -1,14 +1,15 @@
-# Adapted from: https://github.com/pytorch/opacus/blob/master/examples/imdb.py
+# Adapted from: https://github.com/pytorch/opacus/blob/aa31b7399f704a897b9476852e95cbeaf14069be/examples/imdb.py
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
-import torchtext
 from torch import device, optim
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
-from torchtext.data.utils import get_tokenizer
 from tqdm import tqdm
+from transformers import AutoTokenizer
 
+from data import SentimentData
 from model import SentimentAnalysisModel
 
 
@@ -19,20 +20,22 @@ def binary_accuracy(predictions, label):
 
 
 def train(model: SentimentAnalysisModel, train_loader: DataLoader,
-          optimizer: Optimizer, epoch: int):
-    model.train()
+          optimizer: Optimizer, epoch: int, device_: device):
+    model = model.train().to(device_)
     criterion = nn.CrossEntropyLoss()
     losses = []
     accuracies = []
 
     for batch in tqdm(train_loader):
-        data = batch.text.transpose(0, 1)
-        label = batch.label
+        ids = batch['ids'].to(device_, dtype = torch.long)
+        mask = batch['mask'].to(device_, dtype = torch.long)
+        token_type_ids = batch['token_type_ids'].to(device_, dtype = torch.long)
+        targets = batch['targets'].to(device_, dtype = torch.long)
 
         optimizer.zero_grad()
-        predictions = model(data).squeeze(1) # FIXME: Change this to work for transformers (i.e., see forward() method in model)
-        loss = criterion(predictions, label)
-        acc = binary_accuracy(predictions, label)
+        predictions = model(ids, mask, token_type_ids)
+        loss = criterion(predictions, targets)
+        acc = binary_accuracy(predictions, targets)
 
         loss.backward()
         optimizer.step()
@@ -44,20 +47,22 @@ def train(model: SentimentAnalysisModel, train_loader: DataLoader,
     print(f'Train epoch: {epoch} \t Avg Loss: {np.mean(losses)} \t Avg Accuracy: {np.mean(accuracies)}')
 
 
-def evaluate(model: SentimentAnalysisModel, test_loader: DataLoader):
-    model.train()
+def evaluate(model: SentimentAnalysisModel, test_loader: DataLoader, device_: device):
+    model = model.eval().to(device_)
     criterion = nn.CrossEntropyLoss()
     losses = []
     accuracies = []
 
     with torch.no_grad():
         for batch in tqdm(test_loader):
-            data = batch.text.transpose(0, 1)
-            label = batch.label
+            ids = batch['ids'].to(device_, dtype = torch.long)
+            mask = batch['mask'].to(device_, dtype = torch.long)
+            token_type_ids = batch['token_type_ids'].to(device_, dtype = torch.long)
+            targets = batch['targets'].to(device_, dtype = torch.long)
 
-            predictions = model(data).squeeze(1)
-            loss = criterion(predictions, label)
-            acc = binary_accuracy(predictions, label)
+            predictions = model(ids, mask, token_type_ids)
+            loss = criterion(predictions, targets)
+            acc = binary_accuracy(predictions, targets)
 
             losses.append(loss.item())
             accuracies.append(acc.item())
@@ -75,38 +80,42 @@ if __name__ == "__main__":
 
     torch.manual_seed(1234)
     
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device_ = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # load datasets
-    text_field = torchtext.data.Field(
-        tokenize=get_tokenizer("basic_english"),
-        init_token="<sos>",
-        eos_token="<eos>",
-        fix_length=256,
-        lower=True,
+    print('Loading data...')
+    sentiment_data = pd.read_csv('../data/rotten/train.tsv', delimiter='\t')[['Phrase', 'Sentiment']]
+
+    if args.debug:
+        sentiment_data = sentiment_data.sample(n=100)
+
+    train_data = sentiment_data.sample(frac=0.8)
+    test_data = sentiment_data.drop(train_data.index).reset_index(drop=True)
+    train_data = train_data.reset_index(drop=True)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    train_set = SentimentData(train_data, tokenizer, 256)
+    test_set = SentimentData(test_data, tokenizer, 256)
+
+    train_iterator = DataLoader(
+        train_set,
+        batch_size=4,
+        shuffle=True
     )
 
-    label_field = torchtext.data.LabelField(dtype=torch.long)
-
-    train_data, test_data = torchtext.datasets.imdb.IMDB.splits(
-        text_field, label_field, root='../data/'
-    )
-
-    print('Building vocab...')
-    text_field.build_vocab(train_data, max_size=10000)
-    label_field.build_vocab(train_data)
-
-    (train_iterator, test_iterator) = torchtext.data.BucketIterator.splits(
-        (train_data, test_data), batch_size=16, device=device
+    test_iterator = DataLoader(
+        test_set,
+        batch_size=4,
+        shuffle=True
     )
 
     # init model
-    model = SentimentAnalysisModel(args.model, 2)
+    model = SentimentAnalysisModel(args.model, 5).to(device_)
     optimizer = optim.Adam(model.parameters(), lr=1e-05)
 
     # TODO: attach DP to optimizer
 
     print('Training model...')
-    for epoch in range(100):
-        train(model, train_iterator, optimizer, epoch)
-        evaluate(model, test_iterator)
+    for epoch in range(50):
+        train(model, train_iterator, optimizer, epoch, device_)
+        evaluate(model, test_iterator, device_)
